56eae0e3f102b5440b2b4c30f108771851bc59d5
Nikhil R <rnikhil2751996@gmail.com>
2015-03-26 18:54:22 +0530

    Edits

--- api/v0/endpoint/doctor/appointment.php
+++ api/v0/endpoint/doctor/appointment.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$app->get('/doctor/:name/:id/appo', function() use ($app, $connection)
+$app->get('/doctor/:id/appo', function() use ($app, $connection)
 {
 //http://stackoverflow.com/questions/25493942/mysql-how-to-generate-a-complete-calendar-from-an-interval
 // use that link for future use. ITS awesome
@@ -17,6 +17,9 @@ $todate = date("Y-m-d",strtotime("+1 week"));
 // we have $id
 $result = mysqli_query($connection, "select * from  '.$name_$id.' where date BETWEEN '$fromdate' and '$todate'");
 $data   = mysqli_fetch_array($result);
+
+// this will query the doctors table abd give you all appoinbtmentt idd... there is no time specified there...
+
 
   }
 else
