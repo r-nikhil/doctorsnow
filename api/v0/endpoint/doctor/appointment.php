e5e786ab7791fa3caec92b276bbdaaab5f3a865a
Nikhil R <rnikhil2751996@gmail.com>
2015-03-26 18:16:13 +0530

    Check the new schema I put. Autopopulate

--- api/v0/endpoint/doctor/appointment.php
+++ api/v0/endpoint/doctor/appointment.php
@@ -3,11 +3,14 @@
 
 $app->get('/doctor/:id/appo', function() use ($app, $connection)
 {
-  
+
   include "session.php";
   if(isset($login_session_user_patient) || isset($login_session_user_doctor) )
     {
 
+// we have $id
+$result = mysqli_query($connection, "select * from doc_profile where username='$login_session_user_doctor'");
+$data   = mysqli_fetch_array($result);
 
 
   }
